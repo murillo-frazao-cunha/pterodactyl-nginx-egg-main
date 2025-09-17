5ecf7639aa8bd0b3cbcbad6138fbb9c69f536f97
Murillo Frazão Cunha <murillofrazacunha@gmail.com>
2025-09-17 20:28:08 -0300

    Remove unnecessary socket permission adjustments and host entries from nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -6,17 +6,7 @@ PHP_VERSION=$(cat "/etc/enderd/php_version.txt")
 echo "[Docker] Starting PHP-FPM"
 php-fpm$PHP_VERSION -c /etc/enderd/php/php.ini --fpm-config /etc/enderd/php/php-fpm.conf --daemonize
 
-# [Docker] Adjust socket permissions for Nginx to access it
-echo "[Docker] Adjusting permissions"
-chmod 770 /etc/enderd/tmp/php-fpm.sock
-chown www-data:www-data /etc/enderd/tmp/php-fpm.sock
-chown -R www-data:www-data /etc/enderd/www
-chown www-data:www-data /etc/enderd/php/sessions
-
 chmod g+s /etc/enderd/www
-
-echo "10.0.0.190 painel.hight.systems" >> /etc/hosts
-echo "10.0.0.190 node2.hight.systems" >> /etc/hosts
 
 # [Docker] Starting NGINX
 echo "[Docker] Starting NGINX"
